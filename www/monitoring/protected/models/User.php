d0c3493f653fdeefb9e48bb5069e590edee9bf5c
mishapk <mishapk@ya.ru>
2014-09-03 11:29:24 +0300

    Подключено к БД и добавлены пользователи

---
+++ www/monitoring/protected/models/User.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * This is the model class for table "tbl_user".
+ *
+ * The followings are the available columns in table 'tbl_user':
+ * @property integer $id
+ * @property string $username
+ * @property string $password
+ * @property string $email
+ * @property integer $enterprise_id
+ * @property string $role
+ */
+class User extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'tbl_user';
+	}
+//------------------------------------------------------------------------------
+        public function beforeSave()
+        {
+             $this->password=$this->hashPassword($this->password);
+             return parent::beforeSave(); 
+        }        
+//-------------------------------------------------------------------------------  
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('username, password, email, enterprise_id, role', 'required'),
+			array('enterprise_id', 'numerical', 'integerOnly'=>true),
+			array('username, password, email', 'length', 'max'=>128),
+			array('role', 'length', 'max'=>20),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, username, password, email, enterprise_id, role', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'username' => 'Username',
+			'password' => 'Password',
+			'email' => 'Email',
+			'enterprise_id' => 'Enterprise',
+			'role' => 'Role',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('username',$this->username,true);
+		$criteria->compare('password',$this->password,true);
+		$criteria->compare('email',$this->email,true);
+		$criteria->compare('enterprise_id',$this->enterprise_id);
+		$criteria->compare('role',$this->role,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+//-------------------------------------------------------------------------------
+public function validatePassword($password)
+    {
+        return CPasswordHelper::verifyPassword($password,$this->password);
+    }
+ 
+    public function hashPassword($password)
+    {
+        return CPasswordHelper::hashPassword($password);
+    }
+//-------------------------------------------------------------------------------        
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return User the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
